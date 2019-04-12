f5a70ae9713d9878304727f45a9a38bac587de2f
Filip Sijak <filip.sijak@sykescottages.co.uk>
2019-04-12 16:57:45 +0100

    Data for Italy and Germany clubs

--- app/GameEngine/GameData/ClubsByCountry/ClubsGermany.php
+++ app/GameEngine/GameData/ClubsByCountry/ClubsGermany.php
@@ -9,20 +9,166 @@ class ClubsGermany
         return [
             [
                 'name' => 'FC Augsburg',
-                'league_id' => 3,
-                'city' => 1,
-                'country' => 5,
-                'coefficient' => 93, // uefa competitions coefficient
-                'coeff1' => 19, // uefa coefficient for the last year to last five years
-                'coeff2' => 15,
-                'coeff3' => 26,
-                'coeff4' => 18,
-                'coeff5' => 18,
-                'world_ranking' => 8600, // decides popularity of the club, range from 1-10 000, it gives more commercial inc, players wants to come and play here
-                'stadium_id' => 1,
-                'account_balance' => 180 //in milions of euros
+                'coefficient' => 7, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 7,
+                'coeff4' => 0,
+                'coeff5' => 0,
             ],
-            
+            [
+                'name' => 'Borussia Dortmund',
+                'coefficient' => 89, // uefa competitions coefficient
+                'coeff1' => 22, // uefa coefficient for the last year to last five years
+                'coeff2' => 18,
+                'coeff3' => 17,
+                'coeff4' => 22,
+                'coeff5' => 10,
+            ],
+            [
+                'name' => 'FC Bayern Munchen',
+                'coefficient' => 135, // uefa competitions coefficient
+                'coeff1' => 27, // uefa coefficient for the last year to last five years
+                'coeff2' => 28,
+                'coeff3' => 29,
+                'coeff4' => 22,
+                'coeff5' => 29,
+            ],
+            [
+                'name' => 'RB Lepzeg',
+                'coefficient' => 17, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 17,
+            ],
+            [
+                'name' => 'Eintracht Frankfurt',
+                'coefficient' => 12, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 12,
+            ],
+            [
+                'name' => 'Borussia Monchengladbach',
+                'coefficient' => 29, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 9,
+                'coeff3' => 8,
+                'coeff4' => 12,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'VfL Wolfsburg',
+                'coefficient' => 40, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 16,
+                'coeff3' => 24,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'TSG 1899 Hoffenheim',
+                'coefficient' => 4, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 4,
+            ],
+            [
+                'name' => 'SV Werder Bremen',
+                'coefficient' => 0, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'Bayer 04 Leverkusen',
+                'coefficient' => 66, // uefa competitions coefficient
+                'coeff1' => 16, // uefa coefficient for the last year to last five years
+                'coeff2' => 18,
+                'coeff3' => 14,
+                'coeff4' => 18,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'Fortuna Düsseldorf',
+                'coefficient' => 0, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'Hertha Berlin',
+                'coefficient' => 5, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 1,
+                'coeff5' => 4,
+            ],
+            [
+                'name' => 'FSV Mainz 05',
+                'coefficient' => 8, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 1,
+                'coeff3' => 0,
+                'coeff4' => 7,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'Sport-Club Freiburg',
+                'coefficient' => 6, // uefa competitions coefficient
+                'coeff1' => 5, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 1,
+            ],
+            [
+                'name' => 'FC Schalke 04',
+                'coefficient' => 62, // uefa competitions coefficient
+                'coeff1' => 16, // uefa coefficient for the last year to last five years
+                'coeff2' => 17,
+                'coeff3' => 11,
+                'coeff4' => 18,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'VfB Stuttgart',
+                'coefficient' => 2, // uefa competitions coefficient
+                'coeff1' => 2, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'FC Nürnberg',
+                'coefficient' => 0, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'Hannover 96',
+                'coefficient' => 0, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ]
         ];
     }
 }
--- app/GameEngine/GameData/ClubsByCountry/ClubsItaly.php
+++ app/GameEngine/GameData/ClubsByCountry/ClubsItaly.php
@@ -8,21 +8,185 @@ class ClubsGermany
     {
         return [
             [
-                'name' => 'FC Augsburg',
-                'league_id' => 3,
-                'city' => 1,
-                'country' => 5,
-                'coefficient' => 93, // uefa competitions coefficient
-                'coeff1' => 19, // uefa coefficient for the last year to last five years
-                'coeff2' => 15,
-                'coeff3' => 26,
-                'coeff4' => 18,
-                'coeff5' => 18,
-                'world_ranking' => 8600, // decides popularity of the club, range from 1-10 000, it gives more commercial inc, players wants to come and play here
-                'stadium_id' => 1,
-                'account_balance' => 180 //in milions of euros
+                'name' => 'Juventus',
+                'coefficient' => 126, // uefa competitions coefficient
+                'coeff1' => 23, // uefa coefficient for the last year to last five years
+                'coeff2' => 29,
+                'coeff3' => 18,
+                'coeff4' => 33,
+                'coeff5' => 23,
             ],
-            
+            [
+                'name' => 'Napoli',
+                'coefficient' => 78, // uefa competitions coefficient
+                'coeff1' => 16, // uefa coefficient for the last year to last five years
+                'coeff2' => 22,
+                'coeff3' => 13,
+                'coeff4' => 17,
+                'coeff5' => 10,
+            ],
+            [
+                'name' => 'Inter Milan',
+                'coefficient' => 16, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 12,
+                'coeff3' => 0,
+                'coeff4' => 4,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'AC Milan',
+                'coefficient' => 28, // uefa competitions coefficient
+                'coeff1' => 16, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 12,
+            ],
+            [
+                'name' => 'Atalanta',
+                'coefficient' => 11, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 11,
+            ],
+            [
+                'name' => 'AS Roma',
+                'coefficient' => 64, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 12,
+                'coeff3' => 14,
+                'coeff4' => 13,
+                'coeff5' => 25,
+            ],
+            [
+                'name' => 'SS Lazio',
+                'coefficient' => 41, // uefa competitions coefficient
+                'coeff1' => 10, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 14,
+                'coeff4' => 0,
+                'coeff5' => 17,
+            ],
+            [
+                'name' => 'Torino',
+                'coefficient' => 13, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 13,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'UC Sampdoria',
+                'coefficient' => 1, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 1,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'Fiorentina',
+                'coefficient' => 54, // uefa competitions coefficient
+                'coeff1' => 15, // uefa coefficient for the last year to last five years
+                'coeff2' => 20,
+                'coeff3' => 8,
+                'coeff4' => 11,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'US Sassuolo Calcio',
+                'coefficient' => 4, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 4,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'Cagliari',
+                'coefficient' => 0, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'Genoa',
+                'coefficient' => 0, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'Parma',
+                'coefficient' => 0, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'Udinese',
+                'coefficient' => 2, // uefa competitions coefficient
+                'coeff1' => 2, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'SPAL',
+                'coefficient' => 0, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'Bologna',
+                'coefficient' => 0, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'Empoli',
+                'coefficient' => 0, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'Frosinone',
+                'coefficient' => 0, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
+            [
+                'name' => 'Chievo',
+                'coefficient' => 0, // uefa competitions coefficient
+                'coeff1' => 0, // uefa coefficient for the last year to last five years
+                'coeff2' => 0,
+                'coeff3' => 0,
+                'coeff4' => 0,
+                'coeff5' => 0,
+            ],
         ];
     }
 }
