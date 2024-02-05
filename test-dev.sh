3100da8f79d90d2fa251d2e8c17d0c99513f3e0d
Filip <sijakfil@gmail.com>
2024-02-05 18:56:43 +0000

    set filter option on tests

--- test-dev.sh
+++ test-dev.sh
@@ -3,6 +3,4 @@
 php artisan migrate:fresh --database=test-mysql
 php artisan db:seed --database=test-mysql
 
-curl http://localhost:8000/api/setNewGame
-
-php ./vendor/bin/phpunit
+php ./vendor/bin/phpunit $1 $2
