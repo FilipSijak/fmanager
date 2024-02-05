2560bcfe973e8344a9816bbf4f02cced5518ade2
Filip Sijak <filip.sijak@sykescottages.co.uk>
2024-02-05 18:15:15 +0000

    Auth controller cleanup

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,16 +10,16 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $result = $request->validate([
-                                         'name'     => 'required',
-                                         'email'    => 'required|email',
-                                         'password' => 'required|min:6',
-                                     ]);
+            'name'     => 'required',
+            'email'    => 'required|email',
+            'password' => 'required|min:6',
+        ]);
 
         $user = User::create([
-                                 'name'     => $request->name,
-                                 'email'    => $request->email,
-                                 'password' => bcrypt($request->password),
-                             ]);
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
 
         return response()->json($user);
     }
@@ -27,28 +27,28 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-                               'email'    => 'email|required',
-                               'password' => 'required',
-                           ]);
+            'email'    => 'email|required',
+            'password' => 'required',
+        ]);
 
         $credentials = request(['email', 'password']);
 
         if (!auth()->attempt($credentials)) {
             return response()->json([
-                                        'message' => 'The given data was invalid.',
-                                        'errors'  => [
-                                            'password' => [
-                                                'Invalid credentials',
-                                            ],
-                                        ],
-                                    ], 422);
+                'message' => 'The given data was invalid.',
+                'errors'  => [
+                    'password' => [
+                        'Invalid credentials',
+                    ],
+                ],
+            ], 422);
         }
 
         $user      = User::where('email', $request->email)->first();
         $authToken = $user->createToken('auth-token')->plainTextToken;
 
         return response()->json([
-                                    'access_token' => $authToken,
-                                ]);
+            'access_token' => $authToken,
+        ]);
     }
 }
