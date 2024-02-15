56cafc2f14c4597a6bbf7352ffd7b95fc5f37f77
Filip <sijakfil@gmail.com>
2024-02-15 20:12:12 +0000

    Calling transfer service for an even handler in next day subscriber

--- app/Listeners/NexDayTransfersSubscriber.php
+++ app/Listeners/NexDayTransfersSubscriber.php
@@ -3,12 +3,22 @@
 namespace App\Listeners;
 
 use App\Events\NextDay;
+use App\Services\TransferService\TransferService;
 
 class NexDayTransfersSubscriber
 {
+    private TransferService $transferService;
+
+    public function __construct(
+        TransferService $transferService
+    )
+    {
+        $this->transferService = $transferService;
+    }
+
     public function handleProcessTransferRequests()
     {
-
+        $this->transferService->processTransferBids();
     }
 
     public function handleProcessTransferringPlayers($event)
