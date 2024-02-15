65d17090a9e37d1eb90340029f9c5d5221b8ef02
Filip <sijakfil@gmail.com>
2024-02-15 20:13:28 +0000

    Adding Transfer status type enum

---
+++ app/Services/TransferService/TransferStatusTypes.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\TransferService;
+
+// relates to transfer_status table
+enum TransferStatusTypes: int
+{
+    case WAITING_TARGET_CLUB = 1;
+    case WAITING_SOURCE_CLUB = 2;
+    case WAITING_PLAYER = 3;
+    case WAITING_PAPERWORK = 4;
+    case WAITING_TRANSFER_WINDOW = 5;
+    case TRANSFER_COMPLETED = 6;
+}
