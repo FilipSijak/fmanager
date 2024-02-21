c54a5f49ce0d9c847a4c8c3cbae06710d47c9897
Filip <sijakfil@gmail.com>
2024-02-21 18:13:33 +0000

    Adding deployment config json

--- resources/js/FootballManager/Libs/HttpClient.ts
+++ resources/js/FootballManager/Libs/HttpClient.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+import axios, {AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse} from 'axios';
 
 export interface IHttpClientRequestParameters<T> {
     url: string
