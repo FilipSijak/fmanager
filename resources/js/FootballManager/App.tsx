184f5df7e0315052f8024dfe9a32c39d190df853
Filip <sijakfil@gmail.com>
2024-02-22 17:09:29 +0000

    Updating routes

--- resources/js/FootballManager/App.tsx
+++ resources/js/FootballManager/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Provider} from "react-redux";
-import {BrowserRouter, Navigate, Route} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import {AppRoute, routes} from "./routes";
 import Home from "./Modules/HomeScreen/Pages";
 import {store} from "./store";
@@ -10,7 +10,9 @@ export const App: React.FC = () => {
         <React.StrictMode>
             <Provider store={store}>
                 <BrowserRouter>
-                    <Route path="/" element={<Home />} />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
                 </BrowserRouter>
             </Provider>
         </React.StrictMode>
