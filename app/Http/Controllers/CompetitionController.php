74e8084f70b13bd91ec6f88ce5a1203c9071c5ff
Filip <sijakfil@gmail.com>
2024-03-04 17:25:31 +0000

    Competition controller actions

--- app/Http/Controllers/CompetitionController.php
+++ app/Http/Controllers/CompetitionController.php
@@ -2,26 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ResponseHelper;
 use App\Http\Resources\CompetitionResource;
 use App\Models\Competition;
-use Illuminate\Support\Facades\DB;
+use App\Repositories\CompetitionRepository;
+use Illuminate\Http\Request;
 
-class CompetitionController extends Controller
+class CompetitionController extends CoreController
 {
-    public function show(Competition $competition)
+    private CompetitionRepository $competitionRepository;
+
+    public function __construct(
+        Request $request,
+        CompetitionRepository $competitionRepository
+    )
     {
+        parent::__construct($request);
+
+        $this->competitionRepository = $competitionRepository;
+
+        $this->competitionRepository->setInstanceId($this->instanceId);
+        $this->competitionRepository->setSeasonId($this->seasonId);
+    }
+
+    public function show(int $competitionId)
+    {
+        $competition = Competition::where('instance_id', $this->instanceId)
+                                  ->where('id', $competitionId)
+                                  ->first();
+
         return new CompetitionResource($competition);
     }
 
-    public function competitionTable(Competition $competition)
+    public function competitionTable(int $competitionId)
     {
+        $competitionTable = $this->competitionRepository->competitionTable($competitionId);
 
+        return ResponseHelper::success((array)$competitionTable, ResponseHelper::RESPONSE_SUCCESS_CODE);
+    }
 
-        return response()->json(
-            [
-                "data" => [],
-            ],
-            200
-        );
+    public function tournamentGroupsTables(int $competitionId)
+    {
+        $tournamentGroups = $this->competitionRepository->tournamentGroupsTables($competitionId);
+        $tournamentsGroupsMapping = [];
+
+        foreach ($tournamentGroups as $groupItem) {
+            if (!isset($tournamentsGroupsMapping[$groupItem->group_id])) {
+                $tournamentsGroupsMapping[$groupItem->group_id] = [];
+            }
+
+            $tournamentsGroupsMapping[$groupItem->group_id][] = $groupItem;
+        }
+
+        return ResponseHelper::success($tournamentsGroupsMapping, ResponseHelper::RESPONSE_SUCCESS_CODE);
+    }
+
+    public function competitionKnockoutPhase(int $competitionId)
+    {
+        $knockoutSummary = $this->competitionKnockoutPhase($competitionId);
     }
 }
