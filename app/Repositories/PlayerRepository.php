40824460ddf8868727b903a8a4973a421a4394ee
Filip <sijakfil@gmail.com>
2024-09-02 22:28:10 +0100

    Refactor players insert

--- app/Repositories/PlayerRepository.php
+++ app/Repositories/PlayerRepository.php
@@ -14,20 +14,15 @@ class PlayerRepository implements IPlayerRepository
 {
     public function bulkPlayerInsert(int $instanceId, Club $club, array $generatedPlayers)
     {
-        $playerModel = new Player();
-        $columns     = $playerModel->getTableColumns();
         $playerDataSource = new PlayerDataSource();
 
-        unset($columns[0]);
-
-        $playerInsertSQL = "INSERT INTO players(" . implode(", ", $columns) . ") VALUES";
-
-        foreach ($generatedPlayers as $key => $player) {
+        foreach ($generatedPlayers as $player) {
 
             $attributesCategories = $player->getAttributeCategoriesPotential();
             $playerContractRandomEndingYear =  rand(2024, 2030);
             $contractEndDate = date('Y-m-d', strtotime($playerContractRandomEndingYear . '-06-01'));
             $value = 0;
+            $clubRank = $club->rank * 10;
 
             for ($k = 0.1, $i = 10; $i <= 200; $i +=10, $k += 0.06) {
                 if ($player->potential > $i) {
@@ -35,79 +30,74 @@ class PlayerRepository implements IPlayerRepository
                 }
 
                 $value = 180 * round(pow($player->potential, $k), 2) * 1000;
-
                 break;
             }
 
-            $clubRank = $club->rank * 10;
             if ($clubRank > $player->potential) {
                 $playerMarketingRank = $player->potential + (($clubRank - $player->potential) / 2);
             } else {
                 $playerMarketingRank = $player->potential - (($player->potential - $clubRank) / 2);
             }
 
-
             $playerContractID = $playerDataSource->createContractForGeneratedPlayerByPotential($player->potential, $player->position, $playerMarketingRank);
 
-            $playerInsertSQL .= "(" . $instanceId . ",
-                " . $club->id . ",
-                " . $playerContractID . ",
-                '" . $value . "',
-                '" . addslashes($player->first_name) . "',
-                '" . addslashes($player->last_name) . "',
-                '" . $playerMarketingRank . "',
-                '" . $player->potential . "',
-                '" . $player->position . "',
-                '" . $player->country_code . "',
-                '" . $player->dob . "',
-                " . $attributesCategories->technical . ",
-                " . $attributesCategories->mental . ",
-                " . $attributesCategories->physical . ",
-                '" . date('Y-m-d') . "',
-                '" . $contractEndDate . "',
-                " . $player->corners . ",
-                " . $player->crossing . ",
-                " . $player->dribbling . ",
-                " . $player->finishing . ",
-                " . $player->first_touch . ",
-                " . $player->freeKick . ",
-                " . $player->heading . ",
-                " . $player->long_shots . ",
-                " . $player->long_throws . ",
-                " . $player->marking . ",
-                " . $player->passing . ",
-                " . $player->penalty_taking . ",
-                " . $player->tackling . ",
-                " . $player->technique . ",
-                " . $player->aggression . ",
-                " . $player->anticipation . ",
-                " . $player->bravery . ",
-                " . $player->composure . ",
-                " . $player->concentration . ",
-                " . $player->creativity . ",
-                " . $player->decisions . ",
-                " . $player->determination . ",
-                " . $player->flair . ",
-                " . $player->leadership . ",
-                " . $player->of_the_ball . ",
-                " . $player->positioning . ",
-                " . $player->teamwork . ",
-                " . $player->workrate . ",
-                " . $player->acceleration . ",
-                " . $player->agility . ",
-                " . $player->balance . ",
-                " . $player->jumping . ",
-                " . $player->natural_fitness . ",
-                " . $player->pace . ",
-                " . $player->stamina . ",
-                " . $player->strength . "), ";
+            DB::table('players')->insert(
+                [
+                    'instance_id' => $instanceId,
+                    'club_id' => $club->id,
+                    'player_contract_id' => $playerContractID,
+                    'value' => $value,
+                    'first_name' => $player->first_name,
+                    'last_name' => $player->last_name,
+                    'marketing_rank' => $playerMarketingRank,
+                    'potential' => $player->potential,
+                    'position' => $player->position,
+                    'country_code' => $player->country_code,
+                    'dob' => $player->dob,
+                    'technical' => $attributesCategories->technical,
+                    'mental' => $attributesCategories->mental,
+                    'physical' => $attributesCategories->physical,
+                    'contract_start' =>  date('Y-m-d'),
+                    'contract_end' => $contractEndDate,
+                    'corners' => $player->corners,
+                    'crossing' => $player->crossing,
+                    'dribbling' => $player->dribbling,
+                    'finishing' => $player->finishing,
+                    'first_touch' => $player->first_touch,
+                    'freeKick' => $player->freeKick,
+                    'heading' => $player->heading,
+                    'long_shots' => $player->long_shots,
+                    'long_throws' => $player->long_throws,
+                    'marking' => $player->marking,
+                    'passing' => $player->passing,
+                    'penalty_taking' => $player->penalty_taking,
+                    'tackling' => $player->tackling,
+                    'technique' => $player->technique,
+                    'aggression' => $player->aggression,
+                    'anticipation' => $player->anticipation,
+                    'bravery' => $player->bravery,
+                    'composure' => $player->composure,
+                    'concentration' => $player->concentration,
+                    'creativity' => $player->creativity,
+                    'decisions' => $player->decisions,
+                    'determination' => $player->determination,
+                    'flair' => $player->flair,
+                    'leadership' => $player->leadership,
+                    'of_the_ball' => $player->of_the_ball,
+                    'positioning' => $player->positioning,
+                    'teamwork' => $player->teamwork,
+                    'workrate' => $player->workrate,
+                    'acceleration' => $player->acceleration,
+                    'agility' => $player->agility,
+                    'balance' => $player->balance,
+                    'jumping' => $player->jumping,
+                    'natural_fitness' => $player->natural_fitness,
+                    'pace' => $player->pace,
+                    'stamina' => $player->stamina,
+                    'strength' => $player->strength,
+                ]
+            );
         }
-
-        $playerInsertSQL = str_replace(["\r", "\n"], '', $playerInsertSQL);
-
-        $playerInsertSQL = substr($playerInsertSQL, 0, -2);
-
-        DB::statement($playerInsertSQL);
     }
 
     /**
