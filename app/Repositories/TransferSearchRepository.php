4c57d0d764ba391762e5691f4e1e3494e682332d
Filip <sijakfil@gmail.com>
2024-11-13 19:22:00 +0000

    Adding search results for club based on position

--- app/Repositories/TransferSearchRepository.php
+++ app/Repositories/TransferSearchRepository.php
@@ -2,12 +2,14 @@
 
 namespace App\Repositories;
 
+use App\Models\Club;
 use App\Models\Instance;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
 class TransferSearchRepository extends CoreRepository
 {
-    public function playersByAttributes(int $clubId, array $searchableAttribute)
+    public function playersByAttributes(Club $club, array $searchableAttribute)
     {
         $instance = Instance::find($this->instanceId);
 
@@ -20,7 +22,7 @@ class TransferSearchRepository extends CoreRepository
                 }
             })
             ->where('p.instance_id', $this->instanceId)
-            ->where('p.club_id', '<>', $clubId)
+            ->where('p.club_id', '<>', $club->id)
             ->where('p.position', '=','CB')
             ->where(function ($query) use($instance){
                 $query->where('t.offer_date', '>', 't.offer_date > DATE_SUB(' . $instance->instance_date . '",INTERVAL 2 YEAR')
@@ -28,5 +30,28 @@ class TransferSearchRepository extends CoreRepository
             })
             ->get();
     }
-}
 
+    public function findPlayersByPositionForClub(Club $club, string $position): Collection
+    {
+        $instance = Instance::find($club->instance_id);
+
+        return DB::table('players AS p')
+            ->select('p.*')
+            ->leftJoin('transfers AS t', function ($query) use ($instance) {
+                $query->on('t.player_id', '=', 'p.id')
+                    ->whereRaw("`t`.`offer_date` > DATE_SUB('" . $instance->instance_date . "', INTERVAL 2 year)");
+            })
+            ->whereNull('t.player_id')
+            ->where('p.instance_id', $instance->id)
+            ->where('p.club_id', '<>', $club->id)
+            ->where('p.position', '=', $position)
+            ->where('p.potential', '>=', $club->rank * 10 - 20)
+            ->orderBy('p.potential', 'desc')
+            ->get();
+    }
+
+    public function filterTopPlayerByPosition(string $position)
+    {
+
+    }
+}
