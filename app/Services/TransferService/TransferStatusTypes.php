8c43dacf2b7b9e2a207bfcbc66a6dae64d2e1f9c
Filip <sijakfil@gmail.com>
2025-09-17 13:41:03 +0100

    Ordering and adding transfer updates options

--- app/Services/TransferService/TransferStatusTypes.php
+++ app/Services/TransferService/TransferStatusTypes.php
@@ -6,20 +6,18 @@ namespace App\Services\TransferService;
 abstract class TransferStatusTypes
 {
     const WAITING_TARGET_CLUB = 1;
-    const WAITING_SOURCE_CLUB = 2;
-    const WAITING_PLAYER = 3;
-    const WAITING_PAPERWORK = 4;
-    const WAITING_TRANSFER_WINDOW = 5;
-    const TRANSFER_COMPLETED = 6;
-    const TRANSFER_FAILED = 7;
-    const TARGET_CLUB_APPROVED = 8;
-    const SOURCE_CLUB_APPROVED = 9;
-    const PLAYER_APPROVED = 10;
-    const SOURCE_CLUB_COUNTEROFFER = 11;
-    const TARGET_CLUB_COUNTEROFFER = 12;
-    const PLAYER_COUNTEROFFER = 13;
-    const PLAYER_DECLINED = 14;
-    const SOURCE_CLUB_DECLINED = 15;
-    const TARGET_CLUB_DECLINED = 16;
-    const MOVE_PLAYER = 17;
+    const WAITING_PLAYER = 2;
+    const WAITING_PAPERWORK = 3;
+    const WAITING_TRANSFER_WINDOW = 4;
+    const MOVE_PLAYER = 5;
+    const SOURCE_CLUB_COUNTEROFFER = 6;
+    const TARGET_CLUB_COUNTEROFFER = 7;
+    const TARGET_CLUB_COUNTEROFFER_ACCEPTED = 8;
+    const PLAYER_COUNTEROFFER = 9;
+    const PLAYER_COUNTEROFFER_ACCEPTED = 10;
+    const SOURCE_CLUB_PLAYER_COUNTEROFFER = 11;
+    const PLAYER_DECLINED = 12;
+    const TARGET_CLUB_DECLINED = 13;
+    const TRANSFER_COMPLETED = 14;
+    const TRANSFER_FAILED = 15;
 }
