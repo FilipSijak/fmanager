406f3c03b61e0c6bc4a871d7d3af411ef8ae3fa7
Filip <sijakfil@gmail.com>
2026-09-12 00:49:05 +0100

    Consolidate stadium capacity migration

--- database/migrations/2020_02_10_194404_create_stadiums_table.php
+++ database/migrations/2020_02_10_194404_create_stadiums_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateStadiumsTable extends Migration
 {
@@ -20,6 +20,7 @@ class CreateStadiumsTable extends Migration
             $table->string('country_code')->nullable();
             $table->integer('city_id')->unsigned()->nullable();
             $table->integer('capacity');
+            $table->unsignedInteger('active_capacity')->default(0);
         });
     }
 
