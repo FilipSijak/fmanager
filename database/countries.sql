d7c76fddc8547fd834dbd624bccadc3fd1c8fa74
Filip <sijakfil@gmail.com>
2026-09-13 19:56:31 +0100

    Seed country economic data

--- database/countries.sql
+++ database/countries.sql
@@ -1,6 +1,6 @@
 INSERT INTO `countries` (`code`, `name`, `ranking`, `population`, `gdp`, `gdpcapita`, `continent`)
-VALUES ('GBR', 'United Kingdom', 100, 59623400, NULL, NULL, 'Europe'),
- ('ESP', 'Spain', 85, 39441700, NULL, NULL, 'Europe'),
- ('DEU', 'Germany', 80, 82164700, NULL, NULL, 'Europe'),
- ('ITA', 'Italy', 75, 57680000, NULL, NULL, 'Europe'),
- ('FRA', 'France', 65, 59225700, NULL, NULL, 'Europe');
+VALUES ('GBR', 'United Kingdom', 100, 59623400, 2883887, 42000, 'Europe'),
+ ('ESP', 'Spain', 85, 39441700, 1363000, 28000, 'Europe'),
+ ('DEU', 'Germany', 80, 82164700, 3701000, 44000, 'Europe'),
+ ('ITA', 'Italy', 75, 57680000, 1879000, 32000, 'Europe'),
+ ('FRA', 'France', 65, 59225700, 2497000, 37000, 'Europe');
